91d1fd951bacbab1028718e3a378206dd257b818
Simon Heath <icefoxen@gmail.com>
2016-11-26 20:11:31 -0500

    Input subsystem is kinda done-ish.
    Only works for keyboard but hey that's all I care about right now right?
    
    Even has a unit test that makes my math isn't completely bonkers!

--- src/input.rs
+++ src/input.rs
@@ -14,9 +14,7 @@
 //! Right now ggez doesn't handle joysticks or controllers
 //! anyway, so.
 
-use std::cmp::PartialOrd;
 use std::hash::Hash;
-use std::collections::BTreeMap;
 use std::collections::HashMap;
 use ggez::event::*;
 
@@ -47,10 +45,10 @@ enum InputEvent {
     MouseButtonEvent,
 }
 
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd, Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 enum InputEffect<Axes, Buttons>
-    where Axes: Eq + Ord + Clone,
-          Buttons: Eq + Ord + Clone
+    where Axes: Eq + Hash + Clone,
+          Buttons: Eq + Hash + Clone
 {
     Axis(Axes, bool),
     Button(Buttons),
@@ -58,59 +56,108 @@ enum InputEffect<Axes, Buttons>
 
 #[derive(Debug)]
 struct AxisStatus {
+    // Where the axis currently is, in [-1, 1]
     position: f64,
-    current_direction: f64,
+    // Where the axis is moving towards.  Possible
+    // values are -1, 0, +1
+    // (or a continuous range for analog devices I guess)
+    direction: f64,
+    // Speed in units per second that the axis
+    // moves towards the target value.
+    acceleration: f64,
+    // Speed in units per second that the axis will
+    // fall back toward 0 if the input stops.
+    gravity: f64,
 }
 
 impl Default for AxisStatus {
     fn default() -> Self {
         AxisStatus {
             position: 0.0,
-            current_direction: 0.0,
+            direction: 0.0,
+            acceleration: 2.0,
+            gravity: 2.0,
         }
     }
 }
 
 #[derive(Debug)]
 pub struct InputManager<Axes, Buttons>
-    where Axes: Eq + Ord + Hash + Clone,
-          Buttons: Eq + Ord + Hash + Clone
+    where Axes: Hash + Eq + Clone,
+          Buttons: Hash + Eq + Clone
 {
     // Once EnumSet is stable it should be used for these
     // instead of BTreeMap. ♥?
     // Binding of keys to input values.
     bindings: HashMap<InputEvent, InputEffect<Axes, Buttons>>,
     // Input state for axes
-    axes: BTreeMap<Axes, AxisStatus>,
+    axes: HashMap<Axes, AxisStatus>,
     // Input states for buttons
-    buttons: BTreeMap<Buttons, bool>,
+    buttons: HashMap<Buttons, bool>,
 }
 
 impl<Axes, Buttons> InputManager<Axes, Buttons>
-    where Axes: Eq + Ord + Hash + Clone,
-          Buttons: Eq + Ord + Hash + Clone
+    where Axes: Eq + Hash + Clone,
+          Buttons: Eq + Hash + Clone
 {
     pub fn new() -> Self {
         InputManager {
             bindings: HashMap::new(),
-            axes: BTreeMap::new(),
-            buttons: BTreeMap::new(),
+            axes: HashMap::new(),
+            buttons: HashMap::new(),
         }
     }
 
+    /// Adds a key binding connecting the given keycode to the given
+    /// logical axis.
     pub fn bind_key_to_axis(mut self, keycode: Keycode, axis: Axes, positive: bool) -> Self {
 
         self.bindings.insert(InputEvent::KeyEvent(keycode),
-                             InputEffect::Axis(axis, positive));
+                             InputEffect::Axis(axis.clone(), positive));
+        self.axes.insert(axis, AxisStatus::default());
         self
     }
 
+    /// Adds a key binding connecting the given keycode to the given
+    /// logical button.
     pub fn bind_key_to_button(mut self, keycode: Keycode, button: Buttons) -> Self {
-        self.bindings.insert(InputEvent::KeyEvent(keycode), InputEffect::Button(button));
+        self.bindings.insert(InputEvent::KeyEvent(keycode),
+                             InputEffect::Button(button.clone()));
+        self.buttons.insert(button, false);
         self
     }
 
+    /// Updates the logical input state based on the actual
+    /// physical input state.  Should be called in your update()
+    /// handler.
+    /// So, it will do things like move the axes and so on.
+    pub fn update(&mut self, dt: f64) {
+        for (_axis, axis_status) in self.axes.iter_mut() {
+            if axis_status.direction != 0.0 {
+                // Accelerate the axis towards the
+                // input'ed direction.
+                let abs_dx = f64::min(axis_status.acceleration * dt,
+                                      1.0 - f64::abs(axis_status.position));
+                let dx = if axis_status.direction > 0.0 {
+                    abs_dx
+                } else {
+                    -abs_dx
+                };
+                axis_status.position += dx;
+            } else {
+                // Gravitate back towards 0.
+                let abs_dx = f64::min(axis_status.gravity * dt, f64::abs(axis_status.position));
+                let dx = if axis_status.position > 0.0 {
+                    -abs_dx
+                } else {
+                    abs_dx
+                };
+                axis_status.position += dx;
+            }
+        }
+    }
 
+    /// This method should get called by your key_down_event handler.
     pub fn update_keydown(&mut self, keycode: Option<Keycode>) {
         if let Some(keycode) = keycode {
             let effect = {
@@ -124,6 +171,7 @@ impl<Axes, Buttons> InputManager<Axes, Buttons>
         }
     }
 
+    /// This method should get called by your key_up_event handler.
     pub fn update_keyup(&mut self, keycode: Option<Keycode>) {
         if let Some(keycode) = keycode {
             let effect = {
@@ -137,20 +185,22 @@ impl<Axes, Buttons> InputManager<Axes, Buttons>
         }
     }
 
+    /// Takes an InputEffect and actually applies it.
     fn update_effect(&mut self, effect: InputEffect<Axes, Buttons>, started: bool) {
         match effect {
             InputEffect::Axis(axis, direction) => {
-                let direction_float = if direction {
-                    1.0
+                let f = || AxisStatus::default();
+                let axis_status = self.axes.entry(axis).or_insert_with(f);
+                if started {
+                    let direction_float = if direction {
+                        1.0
+                    } else {
+                        -1.0
+                    };
+                    axis_status.direction = direction_float;
                 } else {
-                    -1.0
-                };
-                let default_status = AxisStatus {
-                    position: 0.0,
-                    current_direction: direction_float,
-                };
-                let axis_status = self.axes.entry(axis).or_insert(default_status);
-                axis_status.current_direction = direction_float;
+                    axis_status.direction = 0.0;
+                }
             }
             InputEffect::Button(button) => {
                 let button_pressed = self.buttons.entry(button).or_insert(started);
@@ -160,15 +210,17 @@ impl<Axes, Buttons> InputManager<Axes, Buttons>
         }
     }
 
-    pub fn get_axis(&self, axis: &Axes) -> f64 {
-        if let Some(ax) = self.axes.get(axis) {
-            ax.position
-        } else {
-            0.0
-        }
+    pub fn get_axis(&mut self, axis: Axes) -> f64 {
+        let f = || AxisStatus::default();
+        let axis_status = self.axes.entry(axis).or_insert_with(f);
+        axis_status.position
     }
 
-    pub fn get_axis_raw() {}
+    pub fn get_axis_raw(&mut self, axis: Axes) -> f64 {
+        let f = || AxisStatus::default();
+        let axis_status = self.axes.entry(axis).or_insert_with(f);
+        axis_status.direction
+    }
 
     pub fn get_button(&self, axis: &Buttons) -> bool {
         if let Some(pressed) = self.buttons.get(axis) {
@@ -178,13 +230,17 @@ impl<Axes, Buttons> InputManager<Axes, Buttons>
         }
     }
 
+    pub fn get_button_down(&self, axis: &Buttons) -> bool {
+        self.get_button(axis)
+    }
+
+    pub fn get_button_up(&self, axis: &Buttons) -> bool {
+        !self.get_button(axis)
+    }
+
     pub fn mouse_position() {}
 
     pub fn mouse_scroll_delta() {}
-
-    pub fn get_button_down() {}
-
-    pub fn get_button_up() {}
 
     pub fn get_mouse_button() {}
 
@@ -192,7 +248,12 @@ impl<Axes, Buttons> InputManager<Axes, Buttons>
 
     pub fn get_mouse_button_up() {}
 
-    pub fn reset_input_axes() {}
+    pub fn reset_input_axes(&mut self) {
+        for (_axis, axis_status) in self.axes.iter_mut() {
+            axis_status.position = 0.0;
+            axis_status.direction = 0.0;
+        }
+    }
 }
 
 
@@ -204,20 +265,62 @@ mod tests {
     use super::*;
 
     #[ignore(dead_code)]
-    #[derive(Hash, Copy, Clone, Eq, Ord, PartialEq, PartialOrd)]
+    #[derive(Hash, Eq, PartialEq, Copy, Clone, Debug)]
     enum Buttons {
-        Up,
-        Left,
-        Right,
-        Down,
+        A,
+        B,
+        Select,
+        Start,
+    }
+
+
+    #[ignore(dead_code)]
+    #[derive(Hash, Eq, PartialEq, Copy, Clone, Debug)]
+    enum Axes {
+        Horz,
+        Vert,
     }
     #[test]
     fn test_input_events() {
-        let mut im = InputManager::<(), Buttons>::new().bind_key_to_button(Keycode::W, Buttons::Up);
+        let mut im = InputManager::<Axes, Buttons>::new()
+                         .bind_key_to_button(Keycode::Z, Buttons::A)
+                         .bind_key_to_button(Keycode::X, Buttons::B)
+                         .bind_key_to_button(Keycode::Return, Buttons::Start)
+                         .bind_key_to_button(Keycode::RShift, Buttons::Select)
+                         .bind_key_to_axis(Keycode::Up, Axes::Vert, true)
+                         .bind_key_to_axis(Keycode::Down, Axes::Vert, false)
+                         .bind_key_to_axis(Keycode::Left, Axes::Horz, false)
+                         .bind_key_to_axis(Keycode::Right, Axes::Horz, true);
 
-        im.update_keydown(Some(Keycode::W));
-        assert!(im.get_button(&Buttons::Up));
-        im.update_keyup(Some(Keycode::W));
-        assert!(!im.get_button(&Buttons::Up));
+        im.update_keydown(Some(Keycode::Z));
+        assert!(im.get_button(&Buttons::A));
+        assert!(im.get_button_down(&Buttons::A));
+        im.update_keyup(Some(Keycode::Z));
+        assert!(!im.get_button(&Buttons::A));
+        assert!(im.get_button_up(&Buttons::A));
+
+        // Push the 'up' button, watch the axis
+        // increase to 1.0 but not beyond
+        im.update_keydown(Some(Keycode::Up));
+        assert!(im.get_axis_raw(Axes::Vert) > 0.0);
+        while im.get_axis(Axes::Vert) < 0.99 {
+            im.update(0.16);
+            assert!(im.get_axis(Axes::Vert) >= 0.0);
+            assert!(im.get_axis(Axes::Vert) <= 1.0);
+        }
+        // Release it, watch it wind down
+        im.update_keyup(Some(Keycode::Up));
+        while im.get_axis(Axes::Vert) > 0.01 {
+            im.update(0.16);
+            assert!(im.get_axis(Axes::Vert) >= 0.0)
+        }
+
+        // Do the same with the 'down' button.
+        im.update_keydown(Some(Keycode::Down));
+        while im.get_axis(Axes::Vert) > -0.99 {
+            im.update(0.16);
+            assert!(im.get_axis(Axes::Vert) <= 0.0);
+            assert!(im.get_axis(Axes::Vert) >= -1.0);
+        }
     }
 }
