0257f1cd82f3fbbd6eac0ec7cd5c965dc9dbd9b2
Simon Heath <icefoxen@gmail.com>
2016-11-30 09:30:56 -0500

    Starting to work on scene manager.
    So far it appears to just be a GameState that can swap between other
    different GameState's.  Now, how to specify them sanely...

--- src/scene.rs
+++ src/scene.rs
@@ -5,3 +5,42 @@
 //! We also need hooks: Load, unload... more finely grained?
 //! Kinda tricky to separate create/destroy vs. load and unload,
 //! KISS for now.
+
+use ggez;
+use ggez::GameResult;
+use ggez::conf;
+use ggez::game::GameState;
+
+use std::time::Duration;
+
+pub trait Scene: GameState {}
+
+pub struct SceneManager {
+    current_scene: Option<Box<Scene>>,
+    scene_stack: Vec<Box<Scene>>,
+}
+
+
+impl Default for SceneManager {
+    fn default() -> Self {
+        SceneManager {
+            current_scene: None,
+            scene_stack: Vec::default(),
+        }
+    }
+}
+
+impl GameState for SceneManager {
+    fn load(ctx: &mut ggez::Context, conf: &conf::Conf) -> GameResult<Self> {
+        Ok(SceneManager::default())
+    }
+
+
+    fn update(&mut self, ctx: &mut ggez::Context, dt: Duration) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut ggez::Context) -> GameResult<()> {
+        Ok(())
+    }
+}
