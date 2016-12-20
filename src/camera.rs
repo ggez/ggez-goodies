d07e8776b042b8493cb1aed6d737983aef1a0f7d
Simon Heath <icefoxen@gmail.com>
2016-12-20 10:44:41 -0500

    Added trait to draw things easily with cameras.
    Though the actual coordinate math is not implemented yet...  :-P

--- src/camera.rs
+++ src/camera.rs
@@ -3,6 +3,9 @@
 //! so this really just does the coordinate transforms for you.
 
 use ggez;
+use ggez::GameResult;
+use ggez::graphics;
+use ggez::graphics::Drawable;
 use na;
 use super::{Point2, Vector2};
 pub struct Camera {
@@ -32,17 +35,61 @@ impl Camera {
         self.view_center = to;
     }
 
-    pub fn world_to_screen_coords(&self, from: Vector2) -> (u32, u32) {
+    pub fn world_to_screen_coords(&self, from: Vector2) -> (i32, i32) {
         let width = self.screen_width as f64;
         let height = self.screen_height as f64;
         let x = from.x + width / 2.0;
         let y = height - (from.y + height / 2.0);
-        (x as u32, y as u32)
+        (x as i32, y as i32)
     }
 
 
-    pub fn screen_to_world_coords(&self, from: (u32, u32)) -> Vector2 {
+    pub fn screen_to_world_coords(&self, from: (i32, i32)) -> Vector2 {
         let (sx, sy) = from;
         na::zero()
+    }
+
+    fn calculate_dest_rect(&self, location: Vector2, dst_size: (u32, u32)) -> graphics::Rect {
+        let (sx, sy) = self.world_to_screen_coords(location);
+        let (sw, sh) = dst_size;
+        graphics::Rect::new(sx, sy, sw, sh)
+    }
+}
+
+trait CameraDraw
+    where Self: graphics::Drawable
+{
+    fn draw_ex_camera(&mut self,
+                      camera: &Camera,
+                      location: Vector2,
+                      context: &mut ggez::Context,
+                      src: Option<graphics::Rect>,
+                      dst_size: (u32, u32),
+                      angle: f64,
+                      center: Option<graphics::Point>,
+                      flip_horizontal: bool,
+                      flip_vertical: bool)
+                      -> GameResult<()> {
+        let dest_rect = camera.calculate_dest_rect(location, dst_size);
+        self.draw_ex(context,
+                     src,
+                     Some(dest_rect),
+                     angle,
+                     center,
+                     flip_horizontal,
+                     flip_vertical)
+    }
+
+
+    fn draw_camera(&mut self,
+                   camera: &Camera,
+                   location: Vector2,
+                   context: &mut ggez::Context,
+                   src: Option<graphics::Rect>,
+                   dst_size: (u32, u32))
+                   -> GameResult<()> {
+
+        let dest_rect = camera.calculate_dest_rect(location, dst_size);
+        self.draw(context, src, Some(dest_rect))
     }
 }
