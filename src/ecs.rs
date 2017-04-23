a36a9cf3e10309ebdeb6d80256d1465c960b216c
Simon Heath <icefoxen@gmail.com>
2017-04-23 13:03:11 -0400

    Messing about with ECS more, now takes events and returns new events

--- src/ecs.rs
+++ src/ecs.rs
@@ -1,6 +1,7 @@
 use std::fmt::Debug;
 
 use rayon::prelude::*;
+use rayon;
 use anymap;
 
 #[derive(Clone, Default, Debug)]
@@ -16,15 +17,17 @@ pub struct Component {
 
 
 #[derive(Clone, Default, Debug)]
-pub struct VecResource<T> where T: Debug {
+pub struct VecResource<T>
+    where T: Debug
+{
     data: Vec<T>,
 }
 
-impl<T> VecResource<T> where T: Debug {
+impl<T> VecResource<T>
+    where T: Debug
+{
     pub fn new() -> Self {
-        VecResource {
-            data: Vec::new(),
-        }
+        VecResource { data: Vec::new() }
     }
 }
 
@@ -44,33 +47,32 @@ impl World {
         }
     }
 
-    fn register<T>(&mut self) where T: Default + 'static {
+    fn register<T>(&mut self)
+        where T: Default + 'static
+    {
         self.components.insert(T::default());
     }
 
-    fn run<F, T>(&mut self, f: F) where F: Fn(&T) + Sync, T: Debug + Send {
-        println!("Foo");
-        // if let Some(resource) = self.components.get::<VecResource<T>>() {
-        //     for x in  resource.data
-        //         .iter() {
-        //              println!("{:?}", x);
-        //         }
-
-        //     let d: &[T] = &resource.data;
-        //     // d.par_iter();
-        // } else {
-        //     panic!("Tried to run a system on an unknown component type");
-        // }
-
-        let v: &Vec<u32> = &Vec::new();
-        v.par_iter();
+    fn run<F, T, Eoutput, Einput>(&mut self, inputs: Vec<Einput>, f: F) -> Vec<Eoutput>
+        where F: Fn((&T, &Einput)) -> Eoutput + Sync,
+              T: Debug + Send + Sync + 'static,
+              Eoutput: Send + Sync,
+              Einput: Send + Sync
+    {
+        if let Some(resource) = self.components.get::<VecResource<T>>() {
+            let d: &[T] = &resource.data;
+            let mut v = Vec::new();
+            d.par_iter().zip(&inputs).map(f).collect_into(&mut v);
+            v
+        } else {
+            panic!("Tried to run a system on an unknown component type");
+        }
     }
 
     fn create_entity(&mut self) {
         self.entities.push(Entity((0, 0)));
         let vr: &mut VecResource<u32> = self.components.entry().or_insert_with(VecResource::new);
         vr.data.push(self.entities.len() as u32);
-
     }
 }
 
@@ -81,12 +83,19 @@ mod tests {
 
     #[test]
     fn test_world_thingy() {
+        let entity_count = 10;
         let mut w = World::new();
-        for i in 0..10 {
+        for i in 0..entity_count {
             w.create_entity();
         }
         println!("Bar");
-        w.run(|x:&u32| ());
+        let mut inputs = Vec::new();
+        inputs.resize(entity_count, 1u32);
+        let results = w.run(inputs, |(x1, x2): (&u32, &u32)| {
+            println!("Xs are is {:?} {:?}", x1, x1);
+            x1 + x2
+        });
+        println!("Results are {:?}", results);
         assert!(false);
     }
 }
